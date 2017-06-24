9a5a7dee04d15be8597e502b0df83f3b1201806a
Tanmay <tanmayss@gmail.com>
2017-06-24 13:44:17 -0700

    Implement 0/1 Knapsack with Dynamic Programming.

--- dynamicprogramming/KnapsackProblemZeroOne.java
+++ dynamicprogramming/KnapsackProblemZeroOne.java
@@ -37,11 +37,51 @@ import java.util.Arrays;
  */
 
 public class KnapsackProblemZeroOne {
-	// recursive approach with memoization. 
-	// store solutions to sub-problems and use when needed for same recursive calls. 
-	// this is also a top down approach, where the recursive calls are placed in the function stack, 
-	// till the smaller sub-problems have returned. 
-	// Use a 2-d array to store the results.
+	
+	/* Dynamic programming to implement 0/1 KnapSack Problem.
+	 * It is a bottom-up iterative approach.
+	 * Use a 2 D array to save results of smaller problems, till the given problem is computed 
+	 * using the saved results. 
+	 * Advantage- No recursive function calls, hence recursion overhead avoided.
+	 * Disadvantage- Since all possible sub-problems are computed, hence sometimes results in 
+	 * more subproblems, as compared to the recursive approach where only required sub-problems are computed.
+	 */
+	
+	public static int KnapSackDynamicProgramming(final int n, final int max_c, final int[] W, final int[] V) {
+
+		if (n <= 0 || max_c <= 0)
+			return 0;
+
+		int[][] resultValue = new int[n + 1][max_c + 1];
+		// when n = 0, value = 0
+		// when max = 0, value = 0
+		// initialize the table with -1,
+		for (int i = 1; i < n + 1; ++i) // begin with i = 1, for i = 0 (number of items = 0), value is 0.
+			Arrays.fill(resultValue[i], -1);
+		for (int i = 1; i < n + 1; ++i)
+			resultValue[i][0] = 0; // when max_c = 0, value is 0 for any number items
+
+		int result = 0;
+		for (int item = 1; item < n + 1; ++item) { //n+1 is the length of tghe 2d array
+			for (int wt = 1; wt < max_c + 1; ++wt) {
+				if (W[item - 1] > wt) //For W and V array, the index 'item' is always 1 less.
+					result = resultValue[item - 1][wt];
+				else {
+					result = Math.max(V[item - 1] + resultValue[item - 1][wt - W[item - 1]], resultValue[item - 1][wt]);
+				}
+				resultValue[item][wt] = result;
+			}
+		}
+		return resultValue[n][max_c];
+	}
+	
+	/*****************************************************************************************/
+	
+	/* recursive approach with memoization. 
+	 * store solutions to sub-problems and use when needed for same recursive calls. 
+	 * this is also a top down approach, where the recursive calls are placed in the function stack, 
+	 * till the smaller sub-problems have returned. 
+	 * Use a 2-d array to store the results. */
 	
 	public static int bestValueRecursiveWithMem(int n, int max_w, int[] W, int[] V){
 		
@@ -88,11 +128,12 @@ public class KnapsackProblemZeroOne {
 		return 	result[n][max_w];
 	}
 	
+	/*****************************************************************************************/
 	
+	/* basic recursive approach
+	 * since n is used as the number of items, as well as the index to an array, n=0 is a valid index position
+	 * care has to be taken to make sure w[0] or v[0] is not ignored due to the first if condition */
 	
-	// basic recursive approach
-	// since n is used as the number of items, as well as the index to an array, n=0 is a valid index position
-	// care has to be taken to make sure w[0] or v[0] is not ignored due to the first if condition
 	public static int bestValueRecursive(int itemNumberIndex, int capacity, final int[] W, final int[] V){
 			if (itemNumberIndex < 0 || capacity == 0){
 				return 0; //Value in the knapsack is zero if no items or no capacity
@@ -132,23 +173,27 @@ public class KnapsackProblemZeroOne {
 		return result;
 }
 	
-	//unit testing
+	
+	
+	/* unit testing */
 	public static void main(String [] args){
-		int capacity = 50;
-		int[] V = {60,100,120};
-		int[] W = {10,20,30};
+//		int capacity = 50;
+//		int[] V = {60,100,120};
+//		int[] W = {10,20,30};
 		//answer = 220
 		
-//		int capacity = 15;
-//		int[] V = {7,9,5,12,14,6,12};
-//		int[] W = {3,4,2,6,7,3,5};
+		int capacity = 15;
+		int[] V = {7,9,5,12,14,6,12};
+		int[] W = {3,4,2,6,7,3,5};
 		//answer = 34
 		
 //		int numberOfItems = V.length;
 //		System.out.println("Result = " + bestValueRecursive(numberOfItems-1,capacity,W,V));
 //		System.out.println("Result = " + bestValueRecursive2(0,capacity,W,V)); //calling bestValueRecursive2
 		
-		System.out.println("Result = " + bestValueRecursiveWithMem(V.length,capacity,W,V)); //calling bestValueRecursiveWithMem
+//		System.out.println("Result = " + bestValueRecursiveWithMem(V.length,capacity,W,V)); //calling bestValueRecursiveWithMem
+		
+		System.out.println("Result = " + KnapSackDynamicProgramming(V.length,capacity,W,V)); //calling KnapSackDynamicProgramming
 
 	}
 
